01a2ac2b15d856225c41780172357f855ab7c382
AndeerSU <86616065+AndeerSU@users.noreply.github.com>
2025-04-08 11:58:52 +0200

    Add files via upload

---
+++ Third_checkpoint/python_practical_exercises.py
@@ -0,0 +1,44 @@
+## Ex. 1
+string = 'Jay is here'
+number = 4
+bool = True
+list = ['Harry', 'Asta', 'Dan']
+
+## Ex. 2
+string_letters = string[:2]
+print(string_letters)
+
+## Ex. 3
+first = list[0]
+print(first)
+
+## Ex. 4
+new_numb = number +10
+print(new_numb)
+
+## Ex. 5
+last = list[-1]
+print(last)
+
+## Ex. 6
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(', ')
+print(names_list)
+
+## Ex. 7
+string_upper = string[:2].upper()
+string_upper_conbined = string_upper + string[3:]
+print( string_upper_conbined)
+
+## Ex. 8
+inter_sentence = f"The number {number} is my footbal jersey's number"
+print(inter_sentence)
+
+## Ex. 9
+hello_world = 'hello world'
+print(hello_world)
+
+## Ex. Extra
+hello_world_rep = 'Hello world!'
+bye_world_rep = hello_world_rep.replace('Hello', 'Bye')
+print(bye_world_rep)
