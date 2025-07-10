53c35e475b42e4c987f4028d592b00519c60680b
AndeerSU <86616065+AndeerSU@users.noreply.github.com>
2025-07-10 13:58:27 +0200

    Add files via upload

---
+++ Seventh_checkpoint/Seventh_checkpoint.js
@@ -0,0 +1,15 @@
+
+
+
+function equation(num1, num2, num3, num4) {
+    sum1 = num1 + num2;
+    sum2 = num3 + num4;
+    result = sum1 * sum2;
+    if (result > 50) {
+        console.log('¡El número es mayor que 50!')
+    } else {
+        console.log('¡El número es menor que 50!')
+    }
+}
+
+equation(4, 8, 2, 1)
